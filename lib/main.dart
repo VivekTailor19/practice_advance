4ec8304812d66688a32c823c52202adbdcd11b26
Vivek <tailorvivekgec19@gmail.com>
2023-04-27 10:54:37 +0530

    Show

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,8 @@ import 'package:practice/counter_provider/provider/counter_provider.dart';
 import 'package:practice/counter_provider/view/counter_view.dart';
 import 'package:practice/drawer_n_stepper/gmaillogin.dart';
 import 'package:practice/drawer_n_stepper/stepper_provider.dart';
+import 'package:practice/show_date_time_bottomsheet/show.dart';
+import 'package:practice/show_date_time_bottomsheet/showprovider.dart';
 import 'package:provider/provider.dart';
 
 void main() {
@@ -11,6 +13,7 @@ void main() {
       providers: [
         ChangeNotifierProvider(create: (context) => CounterProvider()),
         ChangeNotifierProvider(create: (context) => StepperProvider()),
+        ChangeNotifierProvider(create: (context) => ShowProvider()),
       ],
 
       child: MaterialApp(
@@ -21,10 +24,11 @@ void main() {
           )
         ),
         debugShowCheckedModeBanner: false,
-        initialRoute: 'gmail',
+        initialRoute: 'show',
         routes: {
           "/":(context) => Counter_UI(),
           "gmail":(context) => Gmail_UI(),
+          "show":(context) => Show_Date_Time_BottomSheet(),
 
 
         },
---
+++ lib/show_date_time_bottomsheet/show.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Show_Date_Time_BottomSheet extends StatefulWidget {
+  const Show_Date_Time_BottomSheet({Key? key}) : super(key: key);
+
+  @override
+  State<Show_Date_Time_BottomSheet> createState() =>
+      _Show_Date_Time_BottomSheetState();
+}
+
+class _Show_Date_Time_BottomSheetState
+    extends State<Show_Date_Time_BottomSheet> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+
+      ),
+    );
+  }
+}
